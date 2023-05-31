1972205a8f592a3a3dbaf5159885e0bcb0f6e95e
Trazee <1056983921@qq.com>
2023-05-31 13:10:08 +0800

    task3/src

---
+++ task3/src/class/langselect.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react'
+import App1 from './app1'
+import App2 from './app2'
+
+export default function Langselect ()
+{
+   
+    const [local, setLocal] = useState(1)
+
+    return (<div>{local === 1 ? <App1 /> : <App2 />}
+    <select onChange={(e) => {
+        console.log('e -->', e.target.value)
+        
+        setLocal(Number(e.target.value))
+    }}>
+        <option value={1}>中文</option>
+        <option value={2}>英文</option>
+    </select>
+    </div>)
+}
--- task3/src/index.js
+++ task3/src/index.js
@@ -1,46 +1,16 @@
-import React from 'react'
 import ReactDOM  from 'react-dom'
+import Langselect from './class/langselect'
+
+
+
 
 ReactDOM.render
 (
-    <div>请输入数字<input type='text' id='unique' ></input><Comparebutton></Comparebutton><Initbutton></Initbutton></div>,document.getElementById('root')
+    <Langselect />,document.getElementById('root')
 )
-var randomnum
-function Initbutton()
-{   
-    var obj={backgroundColor:'blue', color:'white'}
-    function init()
-    {
-    var number = Math.random()
-    number = Math.ceil(number * 100)
-    randomnum=(Number(number))
-    console.log(randomnum)
-    }
-    return (<button style={obj} onClick={init}>重置</button>)
-}
 
-function Comparebutton()
-{   
-    var obj={backgroundColor:'blue', color:'white'}
-    function compare()
-    {   
-        var Byid
-        var inputnum
-        Byid=document.getElementById('unique')
-        inputnum=Number(Byid.value)
-        if(inputnum>randomnum)
-        {
-            alert('大了')
-        }
-        else if(inputnum<randomnum)
-        {
-            alert('小了')
-        }
-        else if(inputnum===randomnum)
-        {
-            alert('猜对了')
-        }
-    }
-    return (<button style={obj} onClick={compare}>对比</button>)
-}
+
+
+
+
 
