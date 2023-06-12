95c7d6577716e2e41b0890b168f8f27c8e192963
ZWQ-Trazee <133731288+ZWQ-Trazee@users.noreply.github.com>
2023-06-12 16:30:04 +0800

    Update app1.js

--- task3/src/class/app1.js
+++ task3/src/class/app1.js
@@ -1,36 +1,22 @@
-import React, { Component } from 'react'
-const initState = {guessnum:'',inputnum:'',output:''} 
-export default class App1 extends Component{   
-    state = {
-        ...initState,
-        guessnum: this.init()
-    }
-    render(){
+import { useState } from "react"
+export default function App1 (){
+    const [guessnum,setGuessnum]=useState(init())
+    const [inputnum,setInputnum]=useState(0)
+    const [output,setOutput]=useState('未比较')
     const obj={backgroundColor:'blue', color:'white'}
-
     return (
     <>请输入一个数字
     <input onChange={(e)=>{
-        console.log(this.state)
-        this.setState({
-            ...this.state,
-            inputnum: Number(e.target.value) 
-        })
-        console.log(this.state.inputnum)}}></input>
+        setInputnum(Number(e.target.value))}}></input>
     <button style={obj} onClick={()=>{
-        this.compare(this.state.inputnum,this.state.guessnum)}}>比对</button>
+        compare(inputnum,guessnum)}}>比对</button>
     <button style={obj} onClick={()=>{
-        this.setState({
-            ...initState,
-            guessnum: this.init()
-        })
-    }}>重置</button>
-    <input type='text' name="result" disabled="disabled" value={this.state.output} />
+        setGuessnum(init())}}>重置</button>
+    <input type='text' name="result" disabled="disabled" value={output} />
     </>
     )
-}
 
-    init(){
+    function init(){
     let numb = Math.random()
     numb = Math.ceil(numb * 100)
     numb=(Number(numb))
@@ -38,30 +24,18 @@ export default class App1 extends Component{
     return numb
     }
 
-    compare(a,b){   
+    function compare(a,b){   
         if(a>b)
         {
-            this.setState({
-                ...this.state,
-                output: '大了'
-            })
-            console.log(this.state.output)
+            setOutput('大了')
         }
         else if(a<b)
         {
-            this.setState({
-                ...this.state,
-                output: '小了'
-            })
-            console.log(this.state.output)
+            setOutput('小了')
         }
         else if(a===b)
         {
-            this.setState({
-                ...this.state,
-                output: '猜对了'
-            })
-            console.log(this.state.output)
+            setOutput('猜对了')
         }
     }
 }
