50ad260c525c5a23a0ed880b19e7e01d0340c24d
Trazee <1056983921@qq.com>
2023-06-14 17:25:15 +0800

    version3

--- task3/.eslintrc.js
+++ task3/.eslintrc.js
@@ -1,26 +1,38 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-  ],
-  rules: {
-    semi: [
-      'error',
-      'never',
-    ],
-  },
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'node': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'overrides': [
+	],
+	'parserOptions': {
+		'ecmaVersion': 'latest',
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		]
+	}
 }
--- task3/src/class/langselect.jsx
+++ task3/src/class/langselect.jsx
@@ -2,21 +2,21 @@ import React, { useState } from 'react'
 import App1 from './app1'
 import App2 from './app2'
 
-export default function Langselect() {
-  const [local, setLocal] = useState(1)
+export default function Langselect () {
+	const [local, setLocal] = useState(1)
+	
 
-  return (
-    <div>
-      {local === 1 ? <App1 /> : <App2 />}
-      <select onChange={(e) => {
-        console.log('e -->', e.target.value)
-
-        setLocal(Number(e.target.value))
-      }}
-      >
-        <option value={1}>中文</option>
-        <option value={2}>英文</option>
-      </select>
-    </div>
-  )
+	return (
+		<div>
+			{local === 1 ? <App1 /> : <App2 />}
+			<select onChange={(e) => {
+				console.log('e -->', e.target.value)
+				setLocal(Number(e.target.value))
+			}}
+			>
+				<option value={1}>中文</option>
+				<option value={2}>英文</option>
+			</select>
+		</div>
+	)
 }
