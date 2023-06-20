2e6adea7aa0b1b7050c7fdc61e752673b86dfb66
Trazee <1056983921@qq.com>
2023-06-20 13:58:50 +0800

    delete task4

--- task4/src/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Tabbarrouter from './router/tabbar-router'
-
-ReactDOM.render(
-	<Tabbarrouter />, document.getElementById('root')
-)
